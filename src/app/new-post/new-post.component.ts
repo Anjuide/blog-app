0c85c36db9825902ca0545a2e51cc4c8510b8516
Anjuide <ange.gnamba@gmail.com>
2019-04-19 22:29:45 +0200

    Nouveau post

---
+++ src/app/new-post/new-post.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PostService } from '../services/post.service';
+import { Router } from '@angular/router';
+import { Post } from '../models/post.model';
+
+@Component({
+  selector: 'app-new-post',
+  templateUrl: './new-post.component.html',
+  styleUrls: ['./new-post.component.scss']
+})
+export class NewPostComponent implements OnInit {
+
+  postForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+              private postService: PostService,
+              private router: Router  ) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+  initForm() {
+    this.postForm = this.formBuilder.group({
+      titre: ['', Validators.required],
+      contenu: ['', Validators.required]
+    });
+  }
+  onSubmitForm() {
+    const formValue = this.postForm.value;
+    const newPost = new Post(
+      formValue['titre'],
+      formValue['contenu']
+    );
+    this.postService.addPost(formValue['titre'], formValue['contenu']);
+    // this.postService.addPost(newPost);
+    this.router.navigate(['/posts']);
+  }
+}
